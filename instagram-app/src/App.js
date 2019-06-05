68db97d4535e82448a5d890872e60fa291a5e06a
chrisdalao <chrisdalao@gmail.com>
2019-06-05 14:05:02 -0700

    added authentication directory with proper imports of new HOC withAuthenticate

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.scss';
 import PostsPage from './components/PostContainer/PostsPage';
+import withAuthenticate from './components/authentication/withAuthenticate';
 
 class App extends React.Component {
-
   render() {
     return (
       <div className="App">
-        <PostsPage />
+        <ComponentFromWithAuthenticate />
       </div>
     );
   }
 
 }
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
+
 
 export default App;
---
+++ instagram-app/src/components/authentication/withAuthenticate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const withAuthenticate = Comp =>
+    class extends React.Component {
+        render() {
+            return (
+                <Comp />
+            )
+        }
+    };
+
+export default withAuthenticate;
